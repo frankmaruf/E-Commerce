9d492c368c82f8ba8394907d7b1a806abad4968e
frankmaruf <frankmaruf@hotmail.com>
2021-05-07 07:30:36 +0600

    Working with cart and Loging

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,7 +4,27 @@ from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer,UserSerializer,UserSerializerWithToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 
 
 
@@ -21,6 +41,19 @@ def getRoutes(request):
         '/api/products/<update>/<id>/',
     ]
     return Response(routes)
+
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+
+    user = request.user
+    serializer = UserSerializer(user,many=False)
+    return Response(serializer.data)
+
+
+
+
 
 @api_view(['GET'])
 def getProducts(request):
